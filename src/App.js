e69cdba6efb03a758d491748ad2ed901961409a1
priyankamorajkar <priyankamorajkar291@gmail.com>
2023-06-22 15:02:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class App extends Component {
+  render(){
+    return <div> Hello from our recipe app </div>;
+  }
+}
+
+export default App;
